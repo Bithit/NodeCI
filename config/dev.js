13a1d86630de6211ff427cf7a7c72d522d43e655
Bithit <nekaposta@gmail.com>
2018-06-30 19:15:37 +0200

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    '584777036620-sf484o2f017v6d0mq4as4dvuckrhtkvq.apps.googleusercontent.com',
-  googleClientSecret: 'nZc78_McYoYKyiJi6HBygOrM',
-  mongoURI: 'mongodb://localhost/blog_dev',
-  // mongoURI: 'mongodb://nekiuser:refref11@ds113849.mlab.com:13849/blog_dev',
-  cookieKey: '4weqrqrf4eef',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
